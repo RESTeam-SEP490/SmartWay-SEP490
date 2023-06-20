496b02a0932c043f05149cc50a1263c2c0b7709c
loris <huytg23112001@gmail.com>
2023-06-20 16:10:23 +0700

    Staff-Management

--- src/main/webapp/app/pages/user/management/users/users.tsx
+++ src/main/webapp/app/pages/user/management/users/users.tsx
@@ -2,9 +2,12 @@ import { Card, Empty, Row, Table, Typography } from 'antd';
 import Column from 'antd/es/table/Column';
 import { useAppDispatch, useAppSelector } from 'app/config/store';
 import { getEntities } from 'app/entities/restaurant/restaurant.reducer';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Translate, translate } from 'react-jhipster';
 import { useLocation, useNavigate } from 'react-router-dom';
+import axios from 'axios';
+
+const listStaffUrl = 'http://localhost:8080/huy/staff';
 
 export const Users = () => {
   const dispatch = useAppDispatch();
@@ -14,19 +17,29 @@ export const Users = () => {
 
   const userList = useAppSelector(state => state.restaurant.entities);
   const loading = useAppSelector(state => state.restaurant.loading);
+  const [staff, setStaff] = useState([]);
 
   useEffect(() => {
-    dispatch(getEntities({}));
+    // dispatch(getEntities({}));
+    const fetchData = async () => {
+      try {
+        axios.get(listStaffUrl).then(response => {
+          setStaff(response.data);
+        });
+      } catch (error) {
+        console.error(error);
+      }
+    };
   }, []);
 
   const handleSyncList = () => {
     dispatch(getEntities({}));
   };
 
-  const users = [
-    { username: 'a', username1: 1 },
-    { username: 'a3werwer', username1: 1 },
-  ];
+  // const users = [
+  //   { username: 'a', username1: 1 },
+  //   { username: 'a3werwer', username1: 1 },
+  // ];
 
   return (
     <div className="flex h-full p-2">
@@ -40,12 +53,16 @@ export const Users = () => {
           </Typography.Title>
         </Row>
         <Table
-          dataSource={users}
+          dataSource={staff}
           rowSelection={{ type: 'checkbox' }}
           locale={{ emptyText: <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} description={translate('global.table.empty')} /> }}
         >
-          <Column title={translate('users.table.columnLabels.username')} dataIndex="username" key="username" />
-          <Column title={translate('users.table.columnLabels.username')} dataIndex="username1" key="username1" />
+          {/*<Column title={translate('users.table.columnLabels.username')} dataIndex="username" key="username" />*/}
+          {/*<Column title={translate('users.table.columnLabels.username')} dataIndex="username1" key="username1" />*/}
+          <Column title="Username" key="username" render={staff => <span>{staff.username}</span>} />
+          <Column title="Full Name" key="fullName" render={staff => <span>{staff.fullName}</span>} />
+          <Column title="Email" key="email" render={staff => <span>{staff.email}</span>} />
+          <Column title="Phone" key="phone" render={staff => <span>{staff.phone}</span>} />
         </Table>
       </div>
     </div>
