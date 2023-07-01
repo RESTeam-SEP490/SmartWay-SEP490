4901462e5d48fad63147257c810ffe2a788aa610
loris <huytg23112001@gmail.com>
2023-07-02 01:17:25 +0700

    export template

--- src/main/webapp/app/entities/menu-item/menu-item.tsx
+++ src/main/webapp/app/entities/menu-item/menu-item.tsx
@@ -1,8 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Translate, translate } from 'react-jhipster';
-
+import { ImportOutlined, WarningOutlined } from '@ant-design/icons';
+import { ExportOutlined } from '@ant-design/icons';
 import { useAppDispatch, useAppSelector } from 'app/config/store';
-
+import { Modal } from 'antd';
 import { DeleteFilled, DeleteOutlined, EditOutlined, PlusOutlined } from '@ant-design/icons';
 import { Button, Card, Empty, Input, Table, Typography } from 'antd';
 import { CheckboxValueType } from 'antd/es/checkbox/Group';
@@ -21,7 +22,11 @@ export const MenuItem = () => {
   const columns = [
     { title: <Translate contentKey="menuItem.code.label" />, dataIndex: 'code', key: 'code' },
     { title: <Translate contentKey="menuItem.name.label" />, dataIndex: 'name', key: 'name' },
-    { title: <Translate contentKey="menuItem.category.label" />, dataIndex: ['menuItemCategory', 'name'], key: 'menuItemCategory' },
+    {
+      title: <Translate contentKey="menuItem.category.label" />,
+      dataIndex: ['menuItemCategory', 'name'],
+      key: 'menuItemCategory',
+    },
     {
       title: <Translate contentKey="menuItem.basePrice.label" />,
       dataIndex: 'basePrice',
@@ -46,6 +51,39 @@ export const MenuItem = () => {
   const count = useAppSelector(state => state.menuItem.totalItems);
   const loading = useAppSelector(state => state.menuItem.loading);
 
+  const [isPopupVisible, setIsPopupVisible] = useState(false);
+
+  const handleOpenPopup = () => {
+    setIsPopupVisible(true);
+  };
+
+  const handleCancelPopup = () => {
+    setIsPopupVisible(false);
+    resetUpload();
+  };
+
+  const resetUpload = () => {
+    setSelectedFile(null);
+    fileInputRef.current.value = null;
+  };
+
+  const [selectedFile, setSelectedFile] = useState(null);
+  const fileInputRef = useRef(null);
+
+  const handleFileSelect = event => {
+    const file = event.target.files[0];
+    setSelectedFile(file);
+  };
+
+  const handleUpload = () => {
+    if (selectedFile) {
+      console.log('Đã chọn tệp:', selectedFile);
+      // Gửi tệp lên máy chủ hoặc thực hiện xử lý khác tùy thuộc vào yêu cầu của bạn
+    } else {
+      console.log('Chưa chọn tệp');
+    }
+  };
+
   useEffect(() => {
     dispatch(getEntities(pageable));
   }, [pageable]);
@@ -67,6 +105,52 @@ export const MenuItem = () => {
     <>
       <MenuItemUpdate handleClose={() => setIsShowForm(false)} isOpen={isShowForm} />
 
+      <Modal visible={isPopupVisible} onCancel={handleCancelPopup} footer={null}>
+        <p>
+          Import Menu Items from Data File. Process Data (Download Sample File:{' '}
+          <a href="http://localhost:8080/downloadTemplate" download>
+            Excel File
+          </a>
+          ).
+        </p>
+        <div style={{ backgroundColor: '#f9f9e0', padding: '10px' }}>
+          <p style={{ marginTop: '10px', fontStyle: 'italic', color: '#7b5e2a' }}>
+            <span style={{ fontWeight: 'bold' }}>
+              <WarningOutlined style={{ marginRight: '0.5rem' }} rev={''} />
+              Note:
+            </span>
+            <br />
+            The system allows a maximum of 100 menu items to be imported from a file at a time.
+            <br />
+            <br />
+            Menu item codes should not contain special characters (@, #, $, *, /, -, _,...) or accented letters, as they may cause
+            difficulties when printing and using barcodes.
+          </p>
+        </div>
+        <div style={{ display: 'flex', justifyContent: 'flex-end', marginTop: '1rem' }}>
+          <div style={{ position: 'relative', marginRight: '10px' }}>
+            <input type="file" onChange={handleFileSelect} style={{ display: 'none' }} ref={fileInputRef} />
+            <div>
+              <Button style={{ float: 'right' }} className="green-button" onClick={() => fileInputRef.current.click()}>
+                Select Data File
+              </Button>
+            </div>
+            {selectedFile && (
+              <div>
+                <div style={{ float: 'right' }}>
+                  <p>{selectedFile.name}</p>
+                </div>
+                <div>
+                  <Button style={{ float: 'right' }} className="green-button" onClick={handleUpload}>
+                    Upload
+                  </Button>
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
+      </Modal>
+
       <div className="flex h-full p-2">
         <div className="flex flex-col w-1/5 gap-4 p-4">
           <Card bordered={false}>
@@ -87,6 +171,12 @@ export const MenuItem = () => {
               <Translate contentKey="menuItem.title" />
             </Typography.Title>
             <div className="flex gap-2">
+              <Button onClick={handleOpenPopup} className="green-button">
+                <span>
+                  <ImportOutlined style={{ marginRight: '0.5rem' }} rev={''} />
+                </span>
+                Import
+              </Button>
               <Button type="primary" onClick={() => setIsShowForm(true)}>
                 <PlusOutlined rev={''} />
                 <Translate contentKey="menuItem.addNewLabel" />
@@ -121,7 +211,9 @@ export const MenuItem = () => {
                 else setExpendedRow(undefined);
               },
             }}
-            locale={{ emptyText: <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} description={translate('global.table.empty')} /> }}
+            locale={{
+              emptyText: <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} description={translate('global.table.empty')} />,
+            }}
           ></Table>
         </div>
       </div>
