ef309362f229770411c0b58c2663cf958787801c
HuyTG <huytg23112001@gmail.com>
2023-07-14 23:44:28 +0700

    update FE + API - List Table

--- src/main/webapp/app/pages/user/selling/orders/order-screen-components/table-list.tsx
+++ src/main/webapp/app/pages/user/selling/orders/order-screen-components/table-list.tsx
@@ -1,58 +1,59 @@
-import React, { useState } from 'react';
-import { Card, Space, Typography } from 'antd';
+import React, { useEffect, useState } from 'react';
+import { Card, Empty, Space, Typography } from 'antd';
 import { ShoppingOutlined, TableOutlined } from '@ant-design/icons';
+import { useAppDispatch, useAppSelector } from 'app/config/store';
+import { getEntities } from 'app/pages/user/management/dining-table/dining-table.reducer';
+import { translate } from 'react-jhipster';
 
 export const TableList = () => {
-  const tables = [
-    { id: 0, name: 'Takeaway' },
-    { id: 1, name: 'Table 1' },
-    { id: 2, name: 'Table 2' },
-    { id: 3, name: 'Table 3' },
-    { id: 4, name: 'Table 4' },
-    { id: 5, name: 'Table 5' },
-    { id: 6, name: 'Table 6' },
-    { id: 7, name: 'Table 7' },
-    { id: 8, name: 'Table 8' },
-    { id: 9, name: 'Table 9' },
-    { id: 10, name: 'Table 10' },
-    { id: 11, name: 'Table 11' },
-    { id: 12, name: 'Table 12' },
-    { id: 13, name: 'Table 13' },
-    { id: 14, name: 'Table 14' },
-    { id: 15, name: 'Table 15' },
-    { id: 16, name: 'Table 16' },
-    { id: 17, name: 'Table 17' },
-    { id: 18, name: 'Table 18' },
-    { id: 19, name: 'Table 19' },
-    { id: 20, name: 'Table 20' },
-  ];
+  const dispatch = useAppDispatch();
+  const diningTableList = useAppSelector(state => state.diningTable.entities);
+
+  useEffect(() => {
+    dispatch(getEntities());
+  }, [dispatch]);
 
   const [selectedTable, setSelectedTable] = useState(null);
-
   const handleTableClick = tableId => {
     setSelectedTable(tableId);
   };
 
+  const sortedDiningTableList = [...diningTableList].sort((a, b) => {
+    if (a.name === 'Takeaway') {
+      return -1;
+    } else if (b.name === 'Takeaway') {
+      return 1;
+    } else {
+      return 0;
+    }
+  });
+
   return (
     <div className="bg-white rounded-e-lg rounded-bl-lg h-screen table-list-container">
-      {tables.map(table => (
-        <Card
-          key={table.id}
-          className={`square-card ${selectedTable === table.id ? 'selected' : ''}`}
-          onClick={() => handleTableClick(table.id)}
-        >
-          <div className="flex-container table-item">
-            <Space direction="vertical" align="center">
-              {table.id === 0 ? (
-                <ShoppingOutlined className="table-outlined-color" rev={undefined} />
-              ) : (
-                <TableOutlined className="table-outlined-color" rev={undefined} />
-              )}
-              <Typography style={{ color: '#0066CC' }}>{table.name}</Typography>
-            </Space>
-          </div>
-        </Card>
-      ))}
+      {sortedDiningTableList.length > 0 ? (
+        sortedDiningTableList.map(table => (
+          <Card
+            key={table.id}
+            className={`square-card ${selectedTable === table.id ? 'selected' : ''}`}
+            onClick={() => handleTableClick(table.id)}
+          >
+            <div className="flex-container table-item">
+              <Space direction="vertical" align="center">
+                {table.name === 'Takeaway' ? (
+                  <ShoppingOutlined className="table-outlined-color" rev={undefined} />
+                ) : (
+                  <TableOutlined className="table-outlined-color" rev={undefined} />
+                )}
+                <Typography style={{ color: '#0066CC' }}>{table.name}</Typography>
+              </Space>
+            </div>
+          </Card>
+        ))
+      ) : (
+        <div className="empty-message">
+          <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} description={translate('global.table.empty')} />
+        </div>
+      )}
     </div>
   );
 };
