ee685070f0139be1d0a0721bacef83d04b261896
HuyTG <huytg23112001@gmail.com>
2023-07-14 22:03:36 +0700

    FE - List Table

--- src/main/webapp/app/pages/user/selling/orders/order-screen-components/table-list.tsx
+++ src/main/webapp/app/pages/user/selling/orders/order-screen-components/table-list.tsx
@@ -1,7 +1,54 @@
 import React from 'react';
+import { Card, Space, Typography } from 'antd';
+import { ShoppingOutlined, TableOutlined } from '@ant-design/icons';
 
 export const TableList = () => {
-  return <div className="bg-white rounded-e-lg rounded-bl-lg h-screen"></div>;
+  const tables = [
+    { id: 1, name: 'Table 1' },
+    { id: 2, name: 'Table 2' },
+    { id: 3, name: 'Table 3' },
+    { id: 4, name: 'Table 4' },
+    { id: 5, name: 'Table 5' },
+    { id: 6, name: 'Table 6' },
+    { id: 7, name: 'Table 7' },
+    { id: 8, name: 'Table 8' },
+    { id: 9, name: 'Table 9' },
+    { id: 10, name: 'Table 10' },
+    { id: 11, name: 'Table 11' },
+    { id: 12, name: 'Table 12' },
+    { id: 13, name: 'Table 13' },
+    { id: 14, name: 'Table 14' },
+    { id: 15, name: 'Table 15' },
+    { id: 16, name: 'Table 16' },
+    { id: 17, name: 'Table 17' },
+    { id: 18, name: 'Table 18' },
+    { id: 19, name: 'Table 19' },
+    { id: 20, name: 'Table 20' },
+  ];
+
+  return (
+    <div className="bg-white rounded-e-lg rounded-bl-lg h-screen table-list-container">
+      <Card className="square-card">
+        <div className="flex-container table-item">
+          <Space direction="vertical" align="center">
+            <ShoppingOutlined rev={undefined} className="table-outlined-color" />
+            <Typography style={{ color: '#0066CC' }}>Takeaway</Typography>
+          </Space>
+        </div>
+      </Card>
+
+      {tables.map(table => (
+        <Card key={table.id} className="square-card">
+          <div className="flex-container table-item">
+            <Space direction="vertical" align="center">
+              <TableOutlined className="table-outlined-color" rev={undefined} />
+              <Typography style={{ color: '#0066CC' }}>{table.name}</Typography>
+            </Space>
+          </div>
+        </Card>
+      ))}
+    </div>
+  );
 };
 
 export default TableList;
--- src/main/webapp/app/pages/user/selling/orders/order.tsx
+++ src/main/webapp/app/pages/user/selling/orders/order.tsx
@@ -1,6 +1,6 @@
 import { BiRestaurant } from 'react-icons/bi';
 import { TableOutlined } from '@ant-design/icons';
-import { Col, Row, Tabs, TabsProps } from 'antd';
+import { Card, Col, Row, Tabs, TabsProps } from 'antd';
 import React from 'react';
 import { Translate } from 'react-jhipster';
 import TableList from './order-screen-components/table-list';
